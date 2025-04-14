9b7a3200c7cc43fed63353821f01930414dae626
sheneeza <126906225+sheneeza@users.noreply.github.com>
2025-04-14 16:39:23 -0400

    finished order microservice, need to fix docker-compose, orderapi get method

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,10 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using OrderAPI.ApplicationCore.Contracts.Services;
+using OrderAPI.ApplicationCore.Entities;
+using OrderAPI.Infrastructure.Data;
+using OrderAPI.Infrastructure.Repositories;
+using OrderAPI.Infrastructure.Services;
+using OrderAPI.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<OrderDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("EShopVAFeb2024"));
+});
+
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(CustomMapper));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderDetailsRepository, OrderDetailsRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderDetailsService, OrderDetailsService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IRepository<PaymentMethod>, BaseRepository<PaymentMethod>>();
+builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.AddScoped<IShoppingCartItemService, ShoppingCartItemService>();
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 
@@ -14,6 +42,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapControllers(); 
+    app.UseRouting();
     app.UseSwagger();
     app.UseSwaggerUI();
     
@@ -39,6 +69,9 @@ app.MapGet("/weatherforecast", () =>
         return forecast;
     })
     .WithName("GetWeatherForecast");
+
+app.MapGet("/test", () => "Order API is running");
+
 
 app.Run();
 
